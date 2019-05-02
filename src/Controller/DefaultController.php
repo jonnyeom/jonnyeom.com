69335025cf0829c8053cbc10176e75422c9cbe76
Jonathan Eom <jonnyeom@gmail.com>
2019-05-02 17:47:37 -0400

    Create a In Progress page and point all non-homepages routes to it

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
@@ -14,6 +15,44 @@ class DefaultController extends AbstractController
     {
         return $this->render('default/homepage.html.twig', [
             'controller_name' => 'DefaultController',
+        ]);
+    }
+
+    /**
+     * @Route("/blog", name="app_blog")
+     */
+    public function blog()
+    {
+        return $this->inProgress('Blog');
+    }
+
+    /**
+     * @Route("/projects", name="app_projects")
+     */
+    public function projects()
+    {
+        return $this->inProgress('Projects');
+    }
+
+    /**
+     * @Route("/about", name="app_about")
+     */
+    public function about()
+    {
+        return $this->inProgress('About');
+    }
+
+    /**
+     * @Route("/in-progress", name="app_in_progress")
+     *
+     * @param string $title
+     * @return Response
+     */
+    private function inProgress($title = 'In Progress'): Response
+    {
+        return $this->render('default/in-progress.html.twig', [
+            'controller_name' => 'DefaultController',
+            'title' => $title,
         ]);
     }
 }
