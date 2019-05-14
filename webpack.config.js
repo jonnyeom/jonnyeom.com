41767b0a1c5f796173e3bcd8647b83ebc919b523
Jonathan Eom <jonnyeom@gmail.com>
2019-05-14 19:33:29 -0400

    Use MiniCssExtract and optimize assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,65 @@
 const path = require('path');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-  entry: './theme/index.js',
-  output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'js/bundle.js'
-  },
-  module: {
-    rules: [{
-      test: /\.scss$/,
-      use: ExtractTextPlugin.extract({
-        fallback: 'style-loader',
-        use: [
-          'css-loader',
-          'sass-loader'
-        ]
-      })
-    }]
-  },
-  plugins: [
-    new ExtractTextPlugin('css/jonnyeom.css'),
-  ]
+    entry: './theme/index.js',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'js/bundle.js'
+    },
+    optimization: {
+        // Extract all CSS in a single file.
+        splitChunks: {
+            cacheGroups: {
+                styles: {
+                    name: 'styles',
+                    test: /\.css$/,
+                    chunks: 'all',
+                    enforce: true,
+                },
+            },
+        },
+        // Minimizing CSS and JS.
+        minimizer: [new TerserPlugin({}), new OptimizeCssAssetsPlugin({
+            cssProcessorPluginOptions: {
+                preset: ['default', {discardComments: {removeAll: true}}],
+            },
+            canPrint: true
+        })],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: 'css/jonnyeom.css',
+            chunkFilename: 'css/[id].css',
+        })
+    ],
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [{
+                loader: MiniCssExtractPlugin.loader,
+                options: {
+                    publicPath: (resourcePath, context) => {
+                        // publicPath is the relative path of the resource to the context
+                        // e.g. for ./css/admin/main.css the publicPath will be ../../
+                        // while for ./css/main.css the publicPath will be ../
+                        return path.relative(path.dirname(resourcePath), context) + '/';
+                    },
+                    hmr: process.env.NODE_ENV === 'development',
+                },
+            }, {
+                loader: "css-loader", options: { // translates CSS into CommonJS modules
+                    // sourceMap: true
+                }
+            }, {
+                loader: "sass-loader", options: { // compiles Sass to CSS
+                    // sourceMap: true
+                }
+            }]
+        }]
+    }
 };
