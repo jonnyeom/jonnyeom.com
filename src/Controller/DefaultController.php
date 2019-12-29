87e09d4d4a9de3d7b0c210c49d13c5ebaba5e969
Jonathan Eom <jonnyeom@gmail.com>
2019-12-28 21:24:40 -0500

    Add Oling

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -55,4 +55,12 @@ class DefaultController extends BaseController
         return $this->inProgress('About');
     }
 
+    /**
+     * @Route("/oling", name="app_oling")
+     */
+    public function oling()
+    {
+        return $this->render('default/oling.html.twig');
+    }
+
 }
