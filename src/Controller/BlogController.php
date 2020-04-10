b12017315fc1fe6da6d7410ebd0c0722ec2ba75d
Jonathan Eom <jonnyeom@gmail.com>
2020-04-09 20:36:38 -0400

    Enable the blog route and individual pages

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -9,29 +9,52 @@ use Symfony\Component\Routing\Annotation\Route;
 class BlogController extends BaseController
 {
     /**
+     * @var array
+     */
+    protected $posts = [
+        'style_guide' => [
+            'title' => 'Style Guide',
+            'date' => 'April 9, 2020',
+            'tags' => [
+                'Drupal',
+                'Test',
+            ],
+        ],
+    ];
+
+    /**
      * @Route("/blog", name="app_blog")
      */
-    public function blog()
+    public function index()
     {
-        return $this->inProgress('Blog');
+        return $this->render('blog/index.html.twig', [
+            'short_title' => 'Example',
+            'posts' => $this->posts,
+        ]);
     }
 
     /**
-     * @Route("/blog/post/example", name="app_post_example")
+     * @Route("/blog/{slug}", name="app_blog_post")
      */
-    public function post(AdapterInterface $cache)
+    public function post($slug, AdapterInterface $cache)
     {
-        $item = $cache->getItem('markdown_post_homepage');
+        // Check if its a valid post.
+        if (empty($this->posts[$slug])) {
+            return $this->redirectToRoute('app_blog');
+        }
+
+        $cid = 'post_' . $slug;
+        $item = $cache->getItem($cid);
         if (!$item->isHit()) {
             $converter = new CommonMarkConverter();
-            $markdown = file_get_contents(__DIR__ . '/../Content/Post/Example.md');
+            $markdown = file_get_contents(__DIR__ . "/../Content/Post/{$slug}.md");
             $item->set($converter->convertToHtml($markdown));
             $cache->save($item);
         }
         $content = $item->get();
 
         return $this->render('blog/post.html.twig', [
-            'short_title' => 'Example',
+            'short_title' => $this->posts[$slug]['title'],
             'content' => $content,
         ]);
     }
