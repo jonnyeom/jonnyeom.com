4fb643682042a13cdf99536634d4ca7434881cf7
Jonathan Eom <jonnyeom@gmail.com>
2022-01-14 10:57:05 -0500

    Update encore dependencies

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,7 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './scss/app.scss';
+import './styles/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
