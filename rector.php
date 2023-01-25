b8d9fcd5a4cb3d0ade043a6c50a96576adbc286e
Jonathan Eom <jonnyeom@gmail.com>
2023-01-25 10:22:21 -0500

    Install rector and phpcs

---
+++ rector.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Symfony\Set\SymfonySetList;
+use Rector\Config\RectorConfig;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->symfonyContainerXml(__DIR__ . '/var/cache/dev/App_KernelDevDebugContainer.xml');
+    $rectorConfig->symfonyContainerPhp(__DIR__ . '/tests/symfony-container.php');
+
+    $rectorConfig->sets([
+        SymfonySetList::SYMFONY_54,
+        SymfonySetList::SYMFONY_CODE_QUALITY,
+        SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
+    ]);
+};
+
