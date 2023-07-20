97b6e7532b80d7e3ac7c985f320bf3da77f7e14d
Jonathan Eom <jonnyeom@gmail.com>
2023-07-20 12:27:36 -0400

    Install test pack with basic web tests

---
+++ tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+final class DefaultControllerTest extends WebTestCase
+{
+    /**
+     * @dataProvider getPublicUrls
+     */
+    public function testPublicUrls(string $url): void
+    {
+        $client = static::createClient();
+        $client->request('GET', $url);
+
+        $this->assertResponseIsSuccessful(sprintf('The %s public URL loads correctly.', $url));
+    }
+
+    public function getPublicUrls(): \Generator
+    {
+        yield ['/'];
+        yield ['/projects'];
+        yield ['/writing'];
+        yield ['/about'];
+    }
+}
