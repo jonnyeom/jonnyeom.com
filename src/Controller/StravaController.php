af3edda83796fc8bca34981fa1ca4b531fb9ca11
Jonathan Eom <jonnyeom@gmail.com>
2024-03-26 18:43:27 -0400

    Add additional strava branding

--- src/Controller/StravaController.php
+++ src/Controller/StravaController.php
@@ -7,23 +7,22 @@ namespace App\Controller;
 use App\Exception\Strava\AccessTokenMissing;
 use App\Service\Strava\StravaDataProvider;
 use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 use function array_reverse;
 
-class StravaController extends AbstractController
+class StravaController extends BaseController
 {
-    public function __construct(private readonly StravaDataProvider $stravaDataProvider)
-    {
-    }
-
     #[Route('/strava', name: 'strava_home')]
-    public function stravaHome(): Response
+    public function stravaHome(StravaDataProvider $stravaDataProvider): Response
     {
+        $this->setSeoTitle('jonnyeom | Weekly running metrics');
+        $this->setSeoDescription('Running metrics based on weekly mileage sourced from Strava');
+        $this->setSeoKeywords('strava,running,metrics,mileage');
+
         try {
-            $runsByWeek = $this->stravaDataProvider->getDataByWeek('Run');
+            $runsByWeek = $stravaDataProvider->getDataByWeek('Run');
         } catch (AccessTokenMissing) {
             return $this->render('strava/home.html.twig');
         } catch (IdentityProviderException $e) {
