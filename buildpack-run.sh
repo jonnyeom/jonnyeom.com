c7916d6edf5112aa3b73ce0c84fbedd55453c9c2
Jonathan Eom <jonnyeom@gmail.com>
2024-04-26 18:39:49 -0400

    Deprecations cleanup

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+# See https://elements.heroku.com/buildpacks/weibeld/heroku-buildpack-run.
+
 echo "Compiling assets to public"
 php bin/console asset-map:compile
 php bin/console debug:asset-map
