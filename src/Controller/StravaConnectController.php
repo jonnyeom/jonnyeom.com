0d4fb0a556c812aa3a4da66913c6284827b013bd
Jonathan Eom <jonnyeom@gmail.com>
2024-09-03 14:40:52 -0400

    Show user auth errors and allow logout

--- src/Controller/StravaConnectController.php
+++ src/Controller/StravaConnectController.php
@@ -6,13 +6,12 @@ namespace App\Controller;
 
 use App\Service\Strava\ClientProvider;
 use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
-use function dd;
 
 class StravaConnectController extends AbstractController
 {
@@ -31,7 +30,7 @@ class StravaConnectController extends AbstractController
     }
 
     #[Route('/strava/connect/check', name: 'strava_connect_check')]
-    public function connectCheckAction(Request $request): Response
+    public function connectCheckAction(Request $request, LoggerInterface $logger): Response
     {
         // ** if you want to *authenticate* the user, then
         // leave this method blank and create a Guard authenticator
@@ -39,17 +38,32 @@ class StravaConnectController extends AbstractController
 
         // @Todo: Add state?
 
+        if (! $code = $request->get('code')) {
+            return $this->redirectToRoute('strava_home');
+        }
+
         try {
-            $code        = $request->get('code');
             $accessToken = $this->clientProvider->getAccessToken($code);
 
-            // Fetch and store the AccessToken
+            // Fetch and store the AccessToken.
             $request->getSession()->set('access_token', $accessToken);
         } catch (IdentityProviderException $e) {
             // something went wrong!
-            // probably you should return the reason to the user
-            dd($e->getMessage());
+            // probably you should return the reason to the user.
+            $logger->error('Strava access error: ' . $e->getMessage());
+
+            return $this->render('strava/home.html.twig', [
+                'error' => $e->getMessage(),
+            ]);
         }
+
+        return $this->redirectToRoute('strava_home');
+    }
+
+    #[Route('/strava/logout', name: 'strava_connect_end')]
+    public function logoutAction(Request $request): Response
+    {
+        $request->getSession()->set('access_token', null);
 
         return $this->redirectToRoute('strava_home');
     }
