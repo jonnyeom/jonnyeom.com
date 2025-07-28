f3c280edf37983ea5a97a91f697f737589dd61d4
Jonathan Eom <jonnyeom@gmail.com>
2025-07-28 14:43:26 -0400

    Remove unused buildpack

--- buildpack-run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# See https://elements.heroku.com/buildpacks/weibeld/heroku-buildpack-run.
-
-echo "Compiling assets to public"
-php bin/console asset-map:compile
-php bin/console debug:asset-map
