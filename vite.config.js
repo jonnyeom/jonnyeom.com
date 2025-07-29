ebd39a83f4718c896236c33c04f84b8ca76aef3c
Jonathan Eom <jonnyeom@gmail.com>
2025-07-29 12:53:41 -0400

    Setup ux-react

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from "vite";
 import symfonyPlugin from "vite-plugin-symfony";
-
-/* if you're using React */
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
         react(),
-        symfonyPlugin(),
+        symfonyPlugin({
+            stimulus: true
+        }),
     ],
     build: {
         sourcemap: true,
