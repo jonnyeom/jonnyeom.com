ac08cf85f88cacd764e63620571f1d2779e669df
Jonathan Eom <jonnyeom@gmail.com>
2025-08-04 15:18:04 -0400

    Update strava scope to only public read

--- src/Controller/StravaConnectController.php
+++ src/Controller/StravaConnectController.php
@@ -31,7 +31,7 @@ class StravaConnectController extends AbstractController
         $oauth = $this->clientProvider->getOAuthClient();
 
         return new RedirectResponse($oauth->getAuthorizationUrl([
-            'scope' => ['read_all', 'profile:read_all', 'activity:read_all'],
+            'scope' => ['read', 'activity:read'],
         ]));
     }
 
